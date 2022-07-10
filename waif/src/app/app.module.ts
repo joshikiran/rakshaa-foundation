bc1139ea3639dd44720dccce96e8c880dfe84848
Joshi <joshik@opentext.com>
2022-07-10 11:26:15 +0530

    components creation !!

--- waif/src/app/app.module.ts
+++ waif/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { WhatWeDoComponent } from './components/what-we-do/what-we-do.component';
+import { WhoWeAreComponent } from './components/who-we-are/who-we-are.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WhatWeDoComponent,
+    WhoWeAreComponent,
+    GalleryComponent,
+    ContactComponent,
+    BlogComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
